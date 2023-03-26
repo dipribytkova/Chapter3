90e1a48f9d00f220d8ddcfb9e545a5be6d439089
Diana Pribytkova <dianababayanarturovna777@gmail.com>
2023-03-26 22:13:15 +0300

    Initial2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int price = 3200;
+        int part = 20;
+        int bonus = 1;
+        System.out.println((price / part * bonus) + " бонусные(ых) мили(ь)");
     }
 }
